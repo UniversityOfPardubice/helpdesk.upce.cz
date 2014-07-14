adf67956e2e1934bb1ae24d8506a343c2be55a1c
lukas-slansky <lukas.slansky@gmail.com>
2014-07-14 09:43:30 +0200

    Vnitřní kódování datového atributu

--- helpdesk-core/src/main/java/org/esupportail/helpdesk/domain/beans/TicketAttribute.java
+++ helpdesk-core/src/main/java/org/esupportail/helpdesk/domain/beans/TicketAttribute.java
@@ -4,28 +4,41 @@
 package org.esupportail.helpdesk.domain.beans;
 
 import java.io.Serializable;
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.esupportail.commons.utils.BeanUtils;
+import org.esupportail.helpdesk.web.beans.CategoryAttributeTypeFormatter;
 
 /**
  * The class that represents ticket attributes.
  */
 public class TicketAttribute implements Serializable {
-    
-	/**
-	 * The serialization id.
-	 */
-    private static final long serialVersionUID = 7061904911180071467L;
+
+    /**
+     * The serialization id.
+     */
+    private static final long serialVersionUID = 7061904911180071468L;
+    private static final String CATEGORY_ATTRIBUTE_TYPE_FORMATTER_NAME = "categoryAttributeTypeFormatter";
+    private static final DateFormat UNIVERSAL_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateFormat[] POSSIBLE_DATE_FORMATS = {
+        new SimpleDateFormat("yyyy-MM-dd"),
+        new SimpleDateFormat("dd.MM.yyyy"),
+        new SimpleDateFormat("dd/MM/yyyy")
+    };
+    private DateFormat outputDateFormat;
 
     /**
      * The ID.
      */
     private long id;
-    
+
     /**
      * The ticket.
      */
     private Ticket ticket;
-    
+
     /**
      * The order of the attribute.
      */
@@ -35,75 +48,83 @@ public class TicketAttribute implements Serializable {
      * The name.
      */
     private String name;
-    
+
     /**
      * The label.
      */
     private String label;
-    
+
     /**
      * The value.
      */
     private String value;
 
-	/**
+    /**
      * Bean constructor.
      */
     public TicketAttribute() {
-    	super();
+        super();
+        try {
+            CategoryAttributeTypeFormatter categoryAttributeTypeFormatter = (CategoryAttributeTypeFormatter) BeanUtils.getBean(CATEGORY_ATTRIBUTE_TYPE_FORMATTER_NAME);
+            outputDateFormat = new SimpleDateFormat(categoryAttributeTypeFormatter.get("DATE_FORMAT"));
+        } catch (Throwable ex) {
+            outputDateFormat = UNIVERSAL_DATE_FORMAT;
+        }
     }
 
     /**
      * Copy constructor.
+     *
      * @param c the ticket attribute to copy
      */
     public TicketAttribute(final TicketAttribute c) {
+        this();
         this.id = c.id;
         this.ticket = c.ticket;
         this.order = c.order;
         this.name = c.name;
-    	this.label = c.label;
+        this.label = c.label;
         this.value = c.value;
     }
 
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(final Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof TicketAttribute)) {
-			return false;
-		}
-		return ((TicketAttribute) obj).getId() == getId();
-	}
-
-	/**
-	 * @see java.lang.Object#hashCode() 
-	 */
-	@Override
-	public int hashCode() {
-		return (int) getId();
-	}
-
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-    public String toString() {
-		return getClass().getSimpleName() + "#" + hashCode() + "[" +
-				"id=[" + id + "]"+
-                ", name=[" + name + "]" +
-                ", label=[" + label + "]" +
-                ", value=[" + value + "]" +
-                "]";
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof TicketAttribute)) {
+            return false;
+        }
+        return ((TicketAttribute) obj).getId() == getId();
     }
 
-	/**
-	 * @return the ID
-	 */
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return (int) getId();
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "#" + hashCode() + "["
+                + "id=[" + id + "]"
+                + ", name=[" + name + "]"
+                + ", label=[" + label + "]"
+                + ", value=[" + value + "]"
+                + "]";
+    }
+
+    /**
+     * @return the ID
+     */
     public long getId() {
         return id;
     }
@@ -183,6 +204,38 @@ public class TicketAttribute implements Serializable {
      */
     public void setValue(String value) {
         this.value = value;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getDateValue() {
+        if (value == null) {
+            return null;
+        }
+        try {
+            return outputDateFormat.format(UNIVERSAL_DATE_FORMAT.parse(value));
+        } catch (ParseException ex) {
+            return null;
+        }
+    }
+
+    protected Date parseDate(String date) {
+        for (DateFormat dateFormat : POSSIBLE_DATE_FORMATS) {
+            try {
+                return dateFormat.parse(date);
+            } catch (ParseException ex) {
+                //Noop
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param value the value
+     */
+    public void setDateValue(String value) {
+        this.value = UNIVERSAL_DATE_FORMAT.format(parseDate(value));
     }
 
 }
--- helpdesk-core/src/main/java/org/esupportail/helpdesk/domain/beans/TicketAttributeData.java
+++ helpdesk-core/src/main/java/org/esupportail/helpdesk/domain/beans/TicketAttributeData.java
@@ -1,4 +1,4 @@
- /**
+/**
  * ESUP-Portail Helpdesk - Copyright (c) 2004-2009 ESUP-Portail consortium.
  */
 package org.esupportail.helpdesk.domain.beans;
@@ -22,153 +22,154 @@ import org.esupportail.commons.services.logging.LoggerImpl;
  * A class to store ticket attribute for displaying.
  */
 public class TicketAttributeData extends TicketAttribute {
-	/**
-	 * The serialization id.
-	 */
-	private static final long serialVersionUID = 7462809945180303544L;
 
-	/**
-	 * The logger.
-	 */
-	private static final Logger LOG = new LoggerImpl(TicketAttributeData.class);
+    /**
+     * The serialization id.
+     */
+    private static final long serialVersionUID = 7462809945180303544L;
 
-	/**
-	 * The type of ticket attribute based on according category attribute.
-	 */
-	private String type;
+    /**
+     * The logger.
+     */
+    private static final Logger LOG = new LoggerImpl(TicketAttributeData.class);
 
-	/**
-	 * Possible values for attribute.
-	 */
-	private List<SelectItem> values;
+    /**
+     * The type of ticket attribute based on according category attribute.
+     */
+    private String type;
 
-	/**
-	 * Default constructor.
-	 */
-	public TicketAttributeData() {
-		super();
-	}
+    /**
+     * Possible values for attribute.
+     */
+    private List<SelectItem> values;
 
-	/**
-	 * Copy constructor to copy from category attribute.
-	 * 
-	 * @param categoryAttribute source category attribute
-	 */
-	TicketAttributeData(CategoryAttribute categoryAttribute) {
-                this.setName(categoryAttribute.getName());
-		this.setLabel(categoryAttribute.getLabel());
-		this.setOrder(categoryAttribute.getOrder());
-		this.setType(categoryAttribute.getType());
+    /**
+     * Default constructor.
+     */
+    public TicketAttributeData() {
+        super();
+    }
 
-		if (CategoryAttribute.DB.equals(categoryAttribute.getType())) {
-			this.setValuesDb(categoryAttribute);
-		}
-		if (CategoryAttribute.SELECT.equals(categoryAttribute.getType())) {
-			this.setValuesSelect(categoryAttribute);
-		}
-	}
+    /**
+     * Copy constructor to copy from category attribute.
+     *
+     * @param categoryAttribute source category attribute
+     */
+    TicketAttributeData(CategoryAttribute categoryAttribute) {
+        this.setName(categoryAttribute.getName());
+        this.setLabel(categoryAttribute.getLabel());
+        this.setOrder(categoryAttribute.getOrder());
+        this.setType(categoryAttribute.getType());
 
-	/**
-	 * Copy constructor.
-	 * 
-	 * @param ticketAttribute source ticket attribute
-	 */
-	public TicketAttributeData(TicketAttribute ticketAttribute) {
-		this.setId(ticketAttribute.getId());
-                this.setName(ticketAttribute.getName());
-		this.setLabel(ticketAttribute.getLabel());
-		this.setOrder(ticketAttribute.getOrder());
-		this.setTicket(ticketAttribute.getTicket());
-		this.setValue(ticketAttribute.getValue());
-	}
+        if (CategoryAttribute.DB.equals(categoryAttribute.getType())) {
+            this.setValuesDb(categoryAttribute);
+        }
+        if (CategoryAttribute.SELECT.equals(categoryAttribute.getType())) {
+            this.setValuesSelect(categoryAttribute);
+        }
+    }
 
-	/**
-	 * @return the type
-	 */
-	public String getType() {
-		return type;
-	}
+    /**
+     * Copy constructor.
+     *
+     * @param ticketAttribute source ticket attribute
+     */
+    public TicketAttributeData(TicketAttribute ticketAttribute) {
+        this.setId(ticketAttribute.getId());
+        this.setName(ticketAttribute.getName());
+        this.setLabel(ticketAttribute.getLabel());
+        this.setOrder(ticketAttribute.getOrder());
+        this.setTicket(ticketAttribute.getTicket());
+        this.setValue(ticketAttribute.getValue());
+    }
 
-	/**
-	 * @param type the type to set
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
 
-	/**
-	 * @return the value
-	 */
-	public List<SelectItem> getValues() {
-		return values;
-	}
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	/**
-	 * Set possible attributes according to DB query. On exception returns empty
-	 * list.
-	 * 
-	 * @param categoryAttribute the category attribute with the JNDI and SQL
-	 * details
-	 */
-	private void setValuesDb(CategoryAttribute categoryAttribute) {
-		values = new ArrayList<SelectItem>();
-		Connection connection = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
-			Context ic = new InitialContext();
-			Context context = (Context) ic.lookup(categoryAttribute
-					.getDbConnectionContext());
-			DataSource dataSource = (DataSource) context
-					.lookup(categoryAttribute.getDbConnectionJndi());
-			connection = dataSource.getConnection();
-			stmt = connection.prepareStatement(categoryAttribute
-					.getDbConnectionSql());
-			rs = stmt.executeQuery();
-			while (rs.next()) {
-				SelectItem value = new SelectItem(rs.getString(1), rs
-						.getString(1));
-				values.add(value);
-			}
-		} catch (Exception ex) {
-			LOG.error(ex);
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException ex) {
-					// nothing to close
-				}
-			}
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException ex) {
-					// nothing to close
-				}
-			}
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException ex) {
-					// nothing to close
-				}
-			}
-		}
-	}
+    /**
+     * @return the value
+     */
+    public List<SelectItem> getValues() {
+        return values;
+    }
 
-	/**
-	 * Set possible attributes according to comma-separated values list
-	 * 
-	 * @param categoryAttribute the category attribute with the list
-	 */
-	private void setValuesSelect(CategoryAttribute categoryAttribute) {
-		String[] attributeValues = categoryAttribute.getValues().split(",");
-		values = new ArrayList<SelectItem>(attributeValues.length);
-		for (String attributeValue : attributeValues) {
-			SelectItem value = new SelectItem(attributeValue.trim(),
-					attributeValue.trim());
-			values.add(value);
-		}
-	}
+    /**
+     * Set possible attributes according to DB query. On exception returns empty
+     * list.
+     *
+     * @param categoryAttribute the category attribute with the JNDI and SQL
+     * details
+     */
+    private void setValuesDb(CategoryAttribute categoryAttribute) {
+        values = new ArrayList<SelectItem>();
+        Connection connection = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            Context ic = new InitialContext();
+            Context context = (Context) ic.lookup(categoryAttribute
+                    .getDbConnectionContext());
+            DataSource dataSource = (DataSource) context
+                    .lookup(categoryAttribute.getDbConnectionJndi());
+            connection = dataSource.getConnection();
+            stmt = connection.prepareStatement(categoryAttribute
+                    .getDbConnectionSql());
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                SelectItem value = new SelectItem(rs.getString(1), rs
+                        .getString(1));
+                values.add(value);
+            }
+        } catch (Exception ex) {
+            LOG.error(ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    // nothing to close
+                }
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    // nothing to close
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    // nothing to close
+                }
+            }
+        }
+    }
+
+    /**
+     * Set possible attributes according to comma-separated values list
+     *
+     * @param categoryAttribute the category attribute with the list
+     */
+    private void setValuesSelect(CategoryAttribute categoryAttribute) {
+        String[] attributeValues = categoryAttribute.getValues().split(",");
+        values = new ArrayList<SelectItem>(attributeValues.length);
+        for (String attributeValue : attributeValues) {
+            SelectItem value = new SelectItem(attributeValue.trim(),
+                    attributeValue.trim());
+            values.add(value);
+        }
+    }
 }
