9dca4ad13d68f3820e35561f815b24d0b9955ed9
Lukas Slansky <lukas.slansky@gmail.com>
2014-09-03 14:32:20 +0200

    Použití vlastního FCK (v budoucnu CK) editoru

---
+++ ck-faces/src/main/java/org/fckfaces/util/Util.java
@@ -0,0 +1,16 @@
+package org.fckfaces.util;
+
+import javax.faces.context.FacesContext;
+
+public class Util {
+	
+	public static final String FCK_FACES_RESOURCE_PREFIX = "/fckfaces";
+	
+	public static final String internalPath(String path) {
+		return FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + FCK_FACES_RESOURCE_PREFIX + path;
+	}
+	
+	public static final String externalPath(String path) {
+		return FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + path;
+	}
+}
